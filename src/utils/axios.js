e22c9dfdd906fdd7909f395753d97f25ed2840ac
s0ulcats <babich.danya30@gmail.com>
2025-01-07 09:41:41 +0200

    fix axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://podocontrolback.onrender.com'
+    baseURL: 'https://podocontrolback.onrender.com/api'
 });
 
 instance.interceptors.request.use(config => {
